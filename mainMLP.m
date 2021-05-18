7bc293c42e02527ed14929071032f6d7ad06d54a
Iman Mousaei <imanmousaei1379@gmail.com>
2021-05-18 21:09:44 +0430

    back propagation

--- mainMLP.m
+++ mainMLP.m
@@ -4,25 +4,15 @@ clear
 close all
 
 
-%% constants
-alpha = 0.1; % learning rate
-
-
-
 %% read data set
 D=xlsread('dataset.xlsx');
-maxn = 100;
-features = zeros(maxn,maxn);
 
-for i=1:size(D, 1)
-    v = D(i,1);
-    u = D(i,1);
-    features(v,u) = 1;
-    features(u,v) = 1;
-    
-end
+%% constants
+alpha = 0.1; % learning rate
+layers = [size(D, 1),10,1]; % number of nodes in each layer
 
-features
+
+
 
 
 
---
+++ wGradient.m
@@ -0,0 +1,17 @@
+function grad = wGradient(i,j,L,a,z,Nlayers,y)
+    % return gradient of MSE loss function to w(L,i,j) ; 
+    % w(L,i,j) : weight of a(L,i) to a(L-1,j)
+    % z : w(L,i).a(L-1,j) + b(L,i)
+    % Nlayers : number of layers
+
+    if L==Nlayers
+        grad = exp(-z) / (1 - exp(-z))^2; % gradient of sigmoid
+    else
+        grad = 1-tanh(z)^2; % gradient of tanh
+    end
+
+    grad = grad * a(L-1,j) * 2*(a(L,i)-y);
+
+end
+
+
