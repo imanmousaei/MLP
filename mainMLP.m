32cecc6bcd1ea2e7b4cd955d571438e5b63a91f4
Iman Mousaei <imanmousaei1379@gmail.com>
2021-05-18 23:38:43 +0430

    confusion matrix

--- mainMLP.m
+++ mainMLP.m
@@ -14,6 +14,9 @@ layers = [size(D,2)-1, 10, 1]; % number of nodes in each layer
 epochs = 10;
 Nlayers = numel(layers);
 maxL = max(size(D,2)-1,10);
+N = size(D,1);
+R = 0.8;
+Ntrain = round(R*N);
 
 a = zeros(Nlayers, maxL);
 z = zeros(Nlayers, maxL);
@@ -31,32 +34,68 @@ for L=2:Nlayers
 end
 
 
-%% evaluate activations
-for L=2:Nlayers
-   for i=1:layers(L)
-       tmp = b(L,i);
-       for j=1:layers(L-1)
-           tmp = tmp + w(L,i,j)*a(L-1,j);
-       end
-       z(L,i) = tmp;
-       a(L,i) = actFcn(tmp,L,Nlayers);
-   end
-end
+%% train
+for ep=1:epochs
+    for row=1:Ntrain
+        %% fill first layer with training data
+        a(1,1:size(D,2)-1) = D(row,1:end-1);
+        y = D(row,end) - 1;
+        
 
-
-%% back propagation
-for L=Nlayers:2
-    for i=1:layers(L)
-        for j=1:layers(L-1)
-           wGrad = wGradient(i,j,L,a,z,Nlayers,y);
-           w(L,i,j) = w(L,i,j) - alpha * wGrad;
+        %% evaluate activations
+        for L=2:Nlayers
+           for i=1:layers(L)
+               tmp = b(L,i);
+               for j=1:layers(L-1)
+                   tmp = tmp + w(L,i,j)*a(L-1,j);
+               end
+               z(L,i) = tmp;
+               a(L,i) = actFcn(tmp,L,Nlayers);
+           end
         end
 
-        bGrad = bGradient(i,L,a,z,Nlayers,y);
-        b(L,i) = b(L,i) - alpha * bGrad;
+        %% back propagation
+        for L=Nlayers:2
+            for i=1:layers(L)
+                for j=1:layers(L-1)
+                   wGrad = wGradient(i,j,L,a,z,Nlayers,y);
+                   w(L,i,j) = w(L,i,j) - alpha * wGrad;
+                end
+
+                bGrad = bGradient(i,L,a,z,Nlayers,y);
+                b(L,i) = b(L,i) - alpha * bGrad;
+            end
+        end
     end
 end
 
+%% test
+for row=Ntrain+1:N
+    %% fill first layer with training data
+    a(1,1:size(D,2)-1) = D(row,1:end-1);
+    y = D(row,end) - 1;
+
+
+    %% evaluate activations
+    for L=2:Nlayers
+       for i=1:layers(L)
+           tmp = b(L,i);
+           for j=1:layers(L-1)
+               tmp = tmp + w(L,i,j)*a(L-1,j);
+           end
+           z(L,i) = tmp;
+           a(L,i) = actFcn(tmp,L,Nlayers);
+       end
+    end
+
+    yHat(row) = a(3,1);
+    yReal(row) = y;
+
+
+end
+
+
+plotconfusion(categorical(yReal),categorical(yHat));
 
 
 
